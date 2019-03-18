714d2293359d4fafed3f19795056344f9b8b02d4
Van Sang Nguyen <vansang.nguyen96@gmail.com>
2019-03-18 22:30:27 +0700

    Adding a Model + Entity Framework + Connection String

---
+++ Movie/Models/Film.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Movie.Models
+{
+    public class Film
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class FilmDBContext : DbContext
+    {
+        public DbSet<Film> Films { get; set; }
+    }
+}
